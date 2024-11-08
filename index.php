3af4d0b3cb3a523f8cb4fe27cb28f66c19795964
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-11-08 04:01:51 -0300

    08NOV24 - Aula 552 - Tipos de requisições (Verbos HTML).

--- index.php
+++ index.php
@@ -1,9 +1,101 @@
 <?php
 
+//use Slim\Http\Request;
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as response;
+
 require 'vendor/autoload.php';
 
 $app = new \Slim\App;
 
+// Padrão Psr7
+$app->get('/postagens', function(Request $request, Response $response) {
+    
+    $response->getBody()->write("Listagem de postagens");
+
+    return $response;
+    //echo "Listagem de postagens";
+
+});
+
+/*
+Tipos de requisição ou Verbos HTTP
+
+get -> Recuperar recursos do servidor (Select)
+post -> Criar dado no servidor (Insert)
+put -> Atualizar dados no servidor (Update)
+delete -> Deletar dados no servidor (Delete)
+
+*/
+
+/////////////////
+// POST METHOD //
+/////////////////
+
+$app->post('/usuarios/adiciona', function(Request $request, Response $response) {
+    
+    // Recuperar post ($_POST)
+    $post = $request->getParsedBody();
+    $nome = $post['nome'];
+    $email = $post['email'];
+
+    /*
+    Salavar no banco de dados com INSERT INTO...
+    ... Lógica ...
+    */
+
+    return $response->getBody()->write(
+        "Nome: " . $nome . " - E-mail: " . $email
+    );
+
+});
+
+////////////////
+// PUT METHOD //
+////////////////
+
+$app->put('/usuarios/atualiza', function(Request $request, Response $response) {
+    
+    // Recuperar post ($_POST)
+    $post = $request->getParsedBody();
+    $id = $post['id'];
+    $nome = $post['nome'];
+    $email = $post['email'];
+
+    /*
+    Atualizar o banco de dados utilizando o UPDATE...
+    ... Lógica ...
+    */
+
+    return $response->getBody()->write(
+        "Sucesso ao atualizar e o ID é: " . $id 
+    );
+
+});
+
+///////////////////
+// DELETE METHOD //
+///////////////////
+
+$app->delete('/usuarios/remove/{id}', function(Request $request, Response $response) {
+    
+    $id = $request->getAttribute('id');
+
+    /*
+    Deletar no banco de dados utilizando o DELETE...
+    ... Lógica ...
+    */
+
+    return $response->getBody()->write(
+        "Sucesso ao deletar e o ID é: " . $id 
+    );
+
+});
+
+
+$app->run();
+
+/*
 $app->get('/postagens-2', function() {
     
     //echo '{"nome": "Papai Cris"}';
@@ -79,8 +171,4 @@ $app->group('/v5', function() {
     });
 
 });
-
-
-
-
-$app->run();
+*/
